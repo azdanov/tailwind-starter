69440371e3d307049e81877e53cae593b94db650
Anton Zdanov <azdanov@protonmail.com>
2018-06-10 06:00:01 +0300

    feat(project): add purgecss example

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-    plugins: [
-        require('tailwindcss')('./tailwind.js')
-    ]
-}
+  plugins: [
+    require('tailwindcss')('./tailwind.js'),
+    purgecss({
+      content: ['**/*.html'],
+      css: ['**/*.css'],
+      whitelist: ['md:flex', 'md:items-center', 'md:mb-0', 'md:mr-4', 'md:text-left'],
+        // whitelistPatterns: [/md:[\w-]+/] // Not Working
+    }),
+  ],
+};
